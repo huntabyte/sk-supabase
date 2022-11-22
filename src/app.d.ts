36fa8823f276ee8fc3a788186bf9b37864e8de91
Hunter Johnston <johnstonhuntera@gmail.com>
2022-11-21 22:29:25 -0500

    initial commit

---
+++ src/app.d.ts
@@ -0,0 +1,21 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+
+import type { TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit/dist/types';
+import type { Session } from '@supabase/supabase-js';
+
+// and what to do when importing types
+declare global {
+	declare namespace App {
+		interface Locals {
+			sb: TypedSupabaseClient;
+			session: Session | null;
+		}
+
+		interface PageData {
+			session: import('@supabase/supabase-js').Session | null;
+		}
+		// interface Error {}
+		// interface Platform {}
+	}
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('flowbite/plugin')],
+
+	darkMode: 'class'
+};
+
+module.exports = config;
